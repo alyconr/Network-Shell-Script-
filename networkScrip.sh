df0a395d3178e2dfa80b5d64a81ca979209c6ba6
alyconr <alyconr@hotmail.com>
2024-02-16 14:00:14 -0500

    Build a feature to check internet connectivity

---
+++ networkScrip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Network testing script  for git bash on Windows
+
+echo "Network Testing Script"
+
+#Check Internet Connectivity
+
+echo "1. Chechinh Internet Connectivity"
+
+ping -c 4 www.google.com
+
+echo "----------------------"
+
